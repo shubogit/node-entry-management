54bf8120dba41f415121784ba065dbe27978da7f
Shubham Kumar <shubo7868@gmail.com>
2019-11-23 22:01:43 +0530

    get entries api, added form for host entry

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const fs = require("fs");
-const path = require("path")
+const path = require("path");
+const uniqid = require("uniqid");
 
 const app = express();
-const PORT = 1200;
+const PORT = process.env.PORT || 1200;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -21,32 +22,59 @@ app.get("/entries", (req, res) => {
 
 app.post("/entry", async (req, res) => {
   let logPath = path.join(__dirname, "logs/entry-log.json");
-  const log = req.body;
+  let log = req.body;
   let logs = [];
   try {
     await fs.readFile(logPath, "utf8", async (err, data) => {
+      let index = null;
       if (err) {
         res.status(500).send("Internal server error");
         throw err;
       }
       if (data !== "") {
         logs = JSON.parse(data);
-        let already = logs.some(entry => entry.email === log.email);
-        if (already) {
-          return res.status(409).send({ message: "Already exist" });
+        index = logs.findIndex(entry => entry.visitor.email === log.visitor.email);
+        // index = index === -1 ? false : index;
+        if (index != -1) {
+          // existing entry
+          logs[index] = {
+            ...log,
+            id: logs[index].id
+          };
+        } else {
+          // new entry
+          log = {
+            ...log,
+            id: uniqid()
+          }
+          logs.push(log);
         }
-        logs.push(log);
       }
       await fs.writeFile(logPath, JSON.stringify(logs), (err, data) => {
         if (err) {
           res.status(500).send("Internal server error");
           throw err;
         }
-        return res.status(201).json(logs);
+        index === -1 ? res.status(201).json({ message: "Entry added successfully" }) : res.status(200).json({ message: "Entry Updated" })
       })
     })
   } catch {
     res.status(500).send("Internal server error");
+  }
+})
+
+app.get("/entry", async (req, res) => {
+  let logPath = path.join(__dirname, "logs/entry-log.json");
+  try {
+    await fs.readFile(logPath, "utf8", async (err, data) => {
+      if (err) {
+        res.status(500).send("Internal server error");
+        throw err;
+      }
+      res.status(200).json(JSON.parse(data))
+    })
+  } catch (error) {
+    res.send(500).send("Internal server error");
   }
 })
 
